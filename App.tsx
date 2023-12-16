74dd1a5f3764ebbefa1c9a4b2d807f26e192db7d
Vuldislav <98829443+vladnory@users.noreply.github.com>
2023-12-16 20:10:38 +0200

    updated eas settings

--- App.tsx
+++ App.tsx
@@ -1,6 +1,7 @@
-import App from "./app/app"
-import React from "react"
+import "expo-dev-client"
 import * as SplashScreen from "expo-splash-screen"
+import React from "react"
+import App from "./app/app"
 
 SplashScreen.preventAutoHideAsync()
 
