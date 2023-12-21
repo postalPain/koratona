96876b7a1aa327027fab2be681f9d3cfb781fcfb
Vuldislav <98829443+vladnory@users.noreply.github.com>
2023-12-21 10:29:46 +0200

    added banner

---
+++ app/components/Banner.tsx
@@ -0,0 +1,29 @@
+import { createUseStyles } from "@stryberventures/gaia-react-native.theme"
+import * as React from "react"
+import { View } from "react-native"
+
+export interface BannerProps {
+  /**
+   * Children components of your component (e.g. <Text />, <View />, etc.)
+   */
+
+  children: React.ReactNode
+}
+
+/**
+ * The banner component is used to display a banner with a background color and padding.
+ * It is used to display a message to the user.
+ */
+export const Banner = (props: BannerProps) => {
+  const styles = useStyles()
+
+  return <View style={styles.container}>{props.children}</View>
+}
+
+const useStyles = createUseStyles((theme) => ({
+  container: {
+    padding: theme.spacing[12],
+    backgroundColor: "#FADFD7",
+    borderRadius: 4,
+  },
+}))
