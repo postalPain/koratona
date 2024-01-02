9ab06e100b576297fd14be6f7b7c048ff109f001
Vuldislav <98829443+vladnory@users.noreply.github.com>
2024-01-02 19:38:10 +0200

    added circleTSXSVG icon

---
+++ assets/icons/svgs/CircleCheckIcon.tsx
@@ -0,0 +1,16 @@
+import * as React from "react"
+import Svg, { Circle, Path } from "react-native-svg"
+
+export default function CircleCheckIconIcon() {
+  return (
+    <Svg width="32" height="30" viewBox="0 0 32 30" fill="none">
+      <Circle cx="15" cy="15" r="15" transform="rotate(180 15 15)" fill="#FADFD7" />
+      <Path
+        fillRule="evenodd"
+        clipRule="evenodd"
+        d="M30.7526 0.663804C31.2145 1.07945 31.2519 1.79077 30.8363 2.25259L17.7299 16.6486C16.268 18.2728 13.7932 18.4753 12.0869 17.1102L6.79726 12.8785C6.31209 12.4904 6.23343 11.7824 6.62156 11.2972C7.0097 10.8121 7.71765 10.7334 8.20282 11.1215L13.4925 15.3532C14.2681 15.9737 15.393 15.8817 16.0574 15.1434L29.1638 0.747424C29.5795 0.285601 30.2908 0.248162 30.7526 0.663804Z"
+        fill="#1A1F51"
+      />
+    </Svg>
+  )
+}
