a54509f2187340a33a9f28327066e06d2c637689
Vuldislav <98829443+vladnory@users.noreply.github.com>
2024-01-04 17:35:30 +0200

    added GoBackComponent

--- app/components/GoBack.tsx
+++ app/components/GoBack.tsx
@@ -1,19 +1,23 @@
 import { createUseStyles } from "@stryberventures/gaia-react-native.theme"
 import React from "react"
-import { Pressable, View } from "react-native"
+import { Pressable, StyleProp, View, ViewStyle } from "react-native"
 import { Icon } from "./Icon"
 import { Text } from "./Text"
 import { typography } from "app/theme"
 
 type Props = {
   onPress: () => void
+  /**
+   * Optional inner header wrapper style override.
+   */
+  style?: StyleProp<ViewStyle>
 }
 
-export const GoBackComponent: React.FC<Props> = ({ onPress }) => {
+export const GoBackComponent: React.FC<Props> = ({ onPress, style: $styleOverride }) => {
   const styles = useStyles()
 
   return (
-    <View style={styles.header}>
+    <View style={[styles.header, $styleOverride]}>
       <Pressable style={styles.leftHeaderComponent} onPress={onPress}>
         <Icon icon="back" color="#667085" />
         <Text text="Go back" style={styles.leftHeaderComponentText} />
