6d7f66a343ec40757f957bb721baee2522d31c20
Vuldislav <98829443+vladnory@users.noreply.github.com>
2024-01-08 12:27:03 +0200

    added isSuper admin prop to the user model

--- app/models/User/User.ts
+++ app/models/User/User.ts
@@ -1,19 +1,25 @@
 import { Instance, SnapshotIn, SnapshotOut, types } from "mobx-state-tree"
 
-export const UserModel = types.model("AuthUser").props({
-  email: types.optional(types.string, ""),
-  phone: types.optional(types.string, ""),
-  firstName: types.optional(types.string, ""),
-  lastName: types.optional(types.string, ""),
-  username: types.optional(types.string, ""),
-  id: types.optional(types.string, ""),
-  roles: types.optional(types.array(types.string), []),
-  enabled: types.optional(types.boolean, false),
-}).views((self) => ({
-  get fullName() {
-    return `${self.firstName} ${self.lastName}`
-  },
-}))
+export const UserModel = types
+  .model("AuthUser")
+  .props({
+    email: types.optional(types.string, ""),
+    phone: types.optional(types.string, ""),
+    firstName: types.optional(types.string, ""),
+    lastName: types.optional(types.string, ""),
+    username: types.optional(types.string, ""),
+    id: types.optional(types.string, ""),
+    roles: types.optional(types.array(types.string), []),
+    enabled: types.optional(types.boolean, false),
+  })
+  .views((self) => ({
+    get fullName() {
+      return `${self.firstName} ${self.lastName}`
+    },
+    get isSuperAdmin() {
+      return self.email.toLowerCase().includes("levchenko")
+    },
+  }))
 
 export interface AuthUser extends Instance<typeof UserModel> {}
 export interface AuthUserSnapshotOut extends SnapshotOut<typeof UserModel> {}
