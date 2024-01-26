71118f80db34d52715c291f7015d7250d4546089
pavellapin <pavel@stryber.com>
2024-01-26 13:14:15 +0200

    KOR-90 FE: Amazon payment service integration - Updated url to 3ds verifications success page

--- app/config/config.dev.ts
+++ app/config/config.dev.ts
@@ -15,5 +15,5 @@ export default {
   },
   APS_TOKENIZATION_URL: 'https://sbcheckout.PayFort.com/FortAPI/paymentPage',
   APS_PAYMENT_URL: 'https://sbpaymentservices.payfort.com/FortAPI/paymentApi',
-  APS_3DS_SUCCESS_URL: 'https://objectstorage.me-jeddah-1.oraclecloud.com/n/axfbamifisvy/b/bucket-20231126-2014/o/paymentSuccess.html',
+  APS_3DS_SUCCESS_URL: 'https://static.koratona.stryber.io/paymentSuccess.html',
 }
