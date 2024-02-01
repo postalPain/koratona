15cab33c75ac665b0c280bfc6233726505c2cbcd
Vuldislav <98829443+vladnory@users.noreply.github.com>
2024-02-01 21:51:43 +0200

    handle restart app after language changed

--- app/i18n/setLanguage.ts
+++ app/i18n/setLanguage.ts
@@ -1,9 +1,11 @@
 import I18n from "i18n-js"
 import { I18nManager } from "react-native"
 import { TLanguage } from "./i18n"
+import * as storage from "app/utils/storage"
 
 export const setLanguage = (language: TLanguage) => {
   I18n.locale = language
+  storage.save("language", language)
 
   const LTR = language === "ar"
   I18nManager.allowRTL(LTR)
