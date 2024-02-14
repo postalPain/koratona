a6dd10bb0a233bdc8b4e623d995ac0e55c888c9e
Pavel Lapin <44573208+pavel-stryber@users.noreply.github.com>
2024-02-14 10:41:40 +0200

    Kor 199 firebase dev (#71)

--- app.config.ts
+++ app.config.ts
@@ -1,4 +1,4 @@
-import { ExpoConfig, ConfigContext } from "@expo/config"
+import { ExpoConfig } from "@expo/config"
 
 /**
  * Use ts-node here so we can use TypeScript for our Config Plugins
@@ -6,13 +6,105 @@ import { ExpoConfig, ConfigContext } from "@expo/config"
  */
 require("ts-node/register")
 
-/**
- * @param config ExpoConfig coming from the static config app.json if it exists
- * 
- * You can read more about Expo's Configuration Resolution Rules here:
- * https://docs.expo.dev/workflow/configuration/#configuration-resolution-rules
- */
-module.exports = ({ config }: ConfigContext): Partial<ExpoConfig> => {
+module.exports = (): Partial<ExpoConfig> => {
+  const DEV_BUILD = process.env.EXPO_PUBLIC_ENV !== 'production';
+  const config: Partial<ExpoConfig> = {
+    "name": "FootballApp",
+    // @ts-ignore
+    "displayName": "Fanfinity",
+    "expo": {
+      "owner": "stryber",
+      "name": "Fanfinity",
+      "slug": "football-app",
+      "scheme": "football-app",
+      "version": "1.2.0",
+      "orientation": "portrait",
+      "icon": "./assets/images/app-icon-all.png",
+      "splash": {
+        "image": "./assets/images/splash-logo-all.png",
+        "resizeMode": "contain",
+        "backgroundColor": "#1973DC"
+      },
+      "updates": {
+        "url": "https://u.expo.dev/d2982e41-5b98-46c8-8e68-f0a553ac5ea4"
+      },
+      "runtimeVersion": "appVersion",
+      "jsEngine": "hermes",
+      "assetBundlePatterns": ["**/*"],
+      "android": {
+        "icon": "./assets/images/app-icon-android-legacy.png",
+        "package": "com.footballappcortona.app",
+        "adaptiveIcon": {
+          "foregroundImage": "./assets/images/app-icon-android-adaptive-foreground.png",
+          "backgroundImage": "./assets/images/app-icon-android-adaptive-background.png"
+        },
+        "splash": {
+          "image": "./assets/images/splash-logo-android-universal.png",
+          "resizeMode": "contain",
+          "backgroundColor": "#1973DC"
+        },
+        "intentFilters": [
+          {
+            "action": "VIEW",
+            "autoVerify": true,
+            "category": ["BROWSABLE", "DEFAULT"]
+          }
+        ],
+        "googleServicesFile": `./google-services${DEV_BUILD ? '_dev' : ''}.json`
+      },
+      "ios": {
+        "icon": "./assets/images/app-icon-ios.png",
+        "supportsTablet": true,
+        "bundleIdentifier": "com.FootballAppCoratona",
+        "splash": {
+          "image": "./assets/images/splash-logo-ios-mobile.png",
+          "tabletImage": "./assets/images/splash-logo-ios-tablet.png",
+          "resizeMode": "contain",
+          "backgroundColor": "#1973DC"
+        },
+        "config": {
+          "usesNonExemptEncryption": false
+        },
+        "googleServicesFile": `./GoogleService-Info${DEV_BUILD ? '_dev' : ''}.plist`
+      },
+      "web": {
+        "favicon": "./assets/images/app-icon-web-favicon.png",
+        "splash": {
+          "image": "./assets/images/splash-logo-web.png",
+          "resizeMode": "contain",
+          "backgroundColor": "#1973DC"
+        },
+        "bundler": "metro"
+      },
+      "plugins": [
+        "expo-localization",
+        [
+          "expo-build-properties",
+          {
+            "ios": {
+              "newArchEnabled": false,
+              "useFrameworks": "static"
+            },
+            "android": {
+              "newArchEnabled": false
+            }
+          }
+        ],
+        "@react-native-firebase/app"
+      ],
+      "experiments": {
+        "tsconfigPaths": true
+      },
+      "extra": {
+        "eas": {
+          "projectId": "d2982e41-5b98-46c8-8e68-f0a553ac5ea4"
+        }
+      }
+    },
+    "ignite": {
+      "version": "9.0.2"
+    }
+  };
   const existingPlugins = config.plugins ?? []
 
   return {
