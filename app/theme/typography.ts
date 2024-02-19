e1ad2268a24ef035d73bc8efd0f4911499edb0fa
Vuldislav <98829443+vladnory@users.noreply.github.com>
2024-02-19 09:49:48 +0200

    added new fonts

--- app/theme/typography.ts
+++ app/theme/typography.ts
@@ -1,14 +1,21 @@
 // TODO: write documentation about fonts and typography along with guides on how to add custom fonts in own
 // markdown file and add links from here
 
-import { Platform } from "react-native"
 import {
-  Inter_300Light as interLight,
-  Inter_400Regular as interRegular,
-  Inter_500Medium as interMedium,
-  Inter_600SemiBold as interSemiBold,
   Inter_700Bold as interBold,
+  Inter_300Light as interLight,
+  Inter_500Medium as interMedium,
+  Inter_400Regular as interRegular,
+  Inter_600SemiBold as interSemiBold,
 } from "@expo-google-fonts/inter"
+import {
+  NotoSansArabic_700Bold as notoSansArabicBold,
+  NotoSansArabic_500Medium as notoSansArabicMedium,
+  NotoSansArabic_400Regular as notoSansArabicRegular,
+  NotoSansArabic_600SemiBold as notoSansArabicSemiBold,
+} from "@expo-google-fonts/noto-sans-arabic"
+import { Platform } from "react-native"
+import { isRTL } from "app/i18n"
 
 // Instrumental Sans
 
@@ -51,6 +58,15 @@ import instrumentalSansCondensedRegularItalic from "assets/fonts/instrumentalSan
 // @ts-ignore
 import instrumentalSansCondensedBoldItalic from "assets/fonts/instrumentalSansCondensed/InstrumentSans_Condensed-BoldItalic.ttf"
 
+// General Sans
+
+// @ts-ignore
+import generalSansRegular from "assets/fonts/generalSans/GeneralSans-Regular.otf"
+// @ts-ignore
+import generalSansMedium from "assets/fonts/generalSans/GeneralSans-Medium.otf"
+// @ts-ignore
+import generalSansSemibold from "assets/fonts/generalSans/GeneralSans-Semibold.otf"
+
 export const customFontsToLoad = {
   interLight,
   interRegular,
@@ -77,6 +93,15 @@ export const customFontsToLoad = {
   instrumentalSansCondensedRegularItalic,
   instrumentalSansCondensedSemiBoldItalic,
   instrumentalSansCondensedBoldItalic,
+  // General Sans,
+  generalSansRegular,
+  generalSansMedium,
+  generalSansSemibold,
+  // Noto Sans Arabic
+  notoSansArabicRegular,
+  notoSansArabicMedium,
+  notoSansArabicSemiBold,
+  notoSansArabicBold,
 }
 
 const fonts = {
@@ -133,6 +158,16 @@ const fonts = {
     bold: "instrumentalSansCondensedBold",
     boldItalic: "instrumentalSansCondensedBoldItalic",
   },
+  generalSans: {
+    regular: "generalSansRegular",
+    medium: "generalSansMedium",
+    semiBold: "generalSansSemibold",
+  },
+  notoSansArabic: {
+    regular: "notoSansArabicRegular",
+    medium: "notoSansArabicMedium",
+    semiBold: "notoSansArabicSemiBold",
+  },
 }
 
 export const typography = {
@@ -152,4 +187,49 @@ export const typography = {
    * Lets get fancy with a monospace font!
    */
   code: Platform.select({ ios: fonts.courier, android: fonts.monospace }),
+}
+
+const fontDependOnLanguage = isRTL() ? "notoSansArabic" : "generalSans"
+
+export const typographyPresets = {
+  "h3-bold": {
+    fontFamily: typography.fonts[fontDependOnLanguage].semiBold,
+    fontSize: 32,
+  },
+  "h4-bold": {
+    fontFamily: typography.fonts[fontDependOnLanguage].semiBold,
+    fontSize: 24,
+  },
+  "p1-regular": {
+    fontFamily: typography.fonts[fontDependOnLanguage].regular,
+    fontSize: 16,
+  },
+  "p1-medium": {
+    fontFamily: typography.fonts[fontDependOnLanguage].medium,
+    fontSize: 16,
+  },
+  "p2-regular": {
+    fontFamily: typography.fonts[fontDependOnLanguage].regular,
+    fontSize: 14,
+  },
+  "p2-medium": {
+    fontFamily: typography.fonts[fontDependOnLanguage].medium,
+    fontSize: 14,
+  },
+  "p2-semibold": {
+    fontFamily: typography.fonts[fontDependOnLanguage].semiBold,
+    fontSize: 14,
+  },
+  "p3-regular": {
+    fontFamily: isRTL() ? typography.fonts.notoSansArabic.regular : typography.fonts.inter.normal,
+    fontSize: 12,
+  },
+  "p3-semibold": {
+    fontFamily: typography.fonts[fontDependOnLanguage].semiBold,
+    fontSize: 12,
+  },
+  "btn1-bold": {
+    fontFamily: typography.fonts[fontDependOnLanguage].semiBold,
+    fontSize: 16,
+  },
 }
